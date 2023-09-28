84c42116bae2d7516773d431b976b519585a9470
paulo <paulo.rmartins@gmail.com>
2023-09-28 17:03:17 +0100

    refactor board animation

--- src/components/nomi/BoardAnimationCanvas.js
+++ src/components/nomi/BoardAnimationCanvas.js
@@ -62,48 +62,59 @@ let requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAni
 let cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame;
 
 
-function useInitCanvas(ref, width, height) {
+function useInitCanvas(canvasRef, ballsRef, width, height, topY, onBallClick) {
+
+  const clearBallsClicked = () => {
+    // Identify which ball is open to be reset
+    let ball = ballsRef.current.find(ball => ball.clicked)
+    if (ball) {
+      ball.clicked = false
+      ball.x = getRandomInt(width / 2, width)
+      ball.y = getRandomInt(0, height)
+      ball.velX = getRandomVel()
+      ball.velY = getRandomVel()
+      ball.radius = ball.originalRadius
+    }
+  }
 
   const handleCanvasOnClick = (e) => {
-    console.log("__handleCanvasOnClick", e);
-    // const {topY} = this.props
-    // const {balls} = this.state
-    // const x = e.pageX - this.canvas.getBoundingClientRect().left
-    // // Note: y should be the position Y of the mouse in the screen but related to the animated board
-    // // XxY at top left corner should be 0x0
-    // // To get Y value we first find the position Y of the mouse and remove all the aggregated height 
-    // // from previous components/pages
-    // // 
-    // const y = e.pageY - topY
-    // // Identify which ball was clicked
-    // let ball = balls.find(ball => Math.sqrt((x-ball.x)*(x-ball.x) + (y-ball.y)*(y-ball.y)) < ball.radius)
-    // if (!!ball) {
-    //   // Reset previous ball selected
-    //   this.clearSelected()
-      
-    //   ball.clicked = true
-    //   this.props.onBallClick(ball.address)
-    // }
+    const x = e.pageX - canvasRef.current.getBoundingClientRect().left
+    // Note: y should be the position Y of the mouse in the screen but related to the animated board
+    // XxY at top left corner should be 0x0
+    // To get Y value we first find the position Y of the mouse and remove all the aggregated height 
+    // from previous components/pages
+    // 
+    const y = e.pageY - topY 
+    // Identify which ball was clicked
+    if (ballsRef.current) {
+      let ball = ballsRef.current.find(ball => Math.sqrt((x-ball.x)*(x-ball.x) + (y-ball.y)*(y-ball.y)) < ball.radius)
+      if (ball) {
+        // Reset previous ball selected
+        clearBallsClicked()
+        
+        ball.clicked = true
+        onBallClick(ball.address)
+      }
+    }
   }
   
   React.useEffect(() => {
-    ref.current.scrollTop = 0;
+    const context = canvasRef.current.getContext('2d')
+    
+    if (context) {
+      canvasRef.current.scrollTop = 0;
 
-    // const canvas = document.getElementById('board')
-    if (ref.current.getContext('2d')) {
-        // Add event listener for `click` events.
-        ref.current.addEventListener('click', handleCanvasOnClick, false);
+      // Add event listener for `click` events.
+      canvasRef.current.addEventListener('click', handleCanvasOnClick, false);
 
-        // set the canvas size
-        ref.current.width = width
-        ref.current.height = height
+      // set the canvas size
+      canvasRef.current.width = width
+      canvasRef.current.height = height
 
     }
 
     return function cleanup() {
-      if (ref.current.getContext('2d')) {
-        ref.current.removeEventListener('click', handleCanvasOnClick, false);
-      }
+      canvasRef.current.removeEventListener('click', handleCanvasOnClick, false);
     }
 
   }, [width, height]);
@@ -111,90 +122,146 @@ function useInitCanvas(ref, width, height) {
   return [];
 }
 
-function useUpdateCanvas(ref, filteredAddresses, currentSelected) {
 
-  // const update = () => {
-  //   const {width, height} = this.props
-  //   const {
-  //     friction,
-  //     ctx,
-  //     balls
-  //   } = this.state
+function useUpdateCanvas(canvasRef, ballsRef, width, height, filteredAddresses, currentSelected) {
+  const [friction, setFriction] = React.useState(0.98);
+  const [context, setContext] = React.useState();
+  // Note: use useRef for mutable variables that we want to persist
+  // without triggering a re-render on their change
+  const requestRef = React.useRef();
 
-  //   this.req = requestAnimationFrame(this.update)
+  const drawBall = (ball) => {
+    context.beginPath()
+    // Define gradient
+    let g = context.createLinearGradient(ball.x-ball.radius, ball.y-ball.radius, ball.x+ball.radius, ball.y+ball.radius);
+    g.addColorStop(0, ball.colorStart);
+    g.addColorStop(1, ball.colorEnd);
+    context.fillStyle = g
+    
+    // Draw the dot
+    context.arc(
+      ball.x, ball.y,
+      ball.radius,
+      0, Math.PI * 2, true
+    )
 
-  //   if (!!ctx) {
-  //     // clear the canvas and redraw everything
-  //     ctx.clearRect(0, 0, width, height)
+    // Sets the type of compositing operation to apply when drawing new shapes
+    if (!ball.clicked) {
+      // ctx.globalCompositeOperation='color-burn';
+      // ctx.globalCompositeOperation='luminosity';
+    }
+    context.globalCompositeOperation='destination-over';
+    // ctx.globalAlpha = 0.8;
 
-  //     balls.forEach((ball) => {
-  //       if (ball.clicked) {
-  //         ball.radius = width / 4.4
-  //         ball.x = (width / 4.4) + 20
-  //         ball.y = (width / 4.4) + 20
-  //       } else {
-  //         if (ball.y + ball.radius >= height) {
-  //           ball.velY *= -ball.bounce
-  //           ball.y = height - ball.radius
-  //           ball.velX *= friction
-  //         }
-  //         if (ball.y - ball.radius <= 0) {
-  //           ball.velY *= -ball.bounce
-  //           ball.y = ball.radius
-  //           ball.velX *= friction
-  //         }
-  //         if (ball.x - ball.radius <= 0) {
-  //           ball.velX *= -ball.bounce
-  //           ball.x = ball.radius
-  //         }
-  //         if (ball.x + ball.radius >= width) {
-  //           ball.velX *= -ball.bounce
-  //           ball.x = width - ball.radius
-  //         }
+    context.fill()
+  }
 
-  //         if (ball.velX < 0.01 && ball.velX > -0.01) {
-  //           ball.velX = 0
-  //         }
-  //         if (ball.velY < 0.01 && ball.velY > -0.01) {
-  //           ball.velY = 0
-  //         }
+  const update = () => {
+    
+    if (context) {
+      // clear the canvas and redraw everything
+      context.clearRect(0, 0, width, height)
 
-  //         ball.x += ball.velX
-  //         ball.y += ball.velY
-  //       }
-  //       this.draw(ball)
-  //     })
-  //   }
-  // }
-
+      ballsRef.current.forEach((ball) => {
+        if (ball.clicked) {
+          ball.radius = width / 4.4
+          ball.x = (width / 4.4) + 20
+          ball.y = (width / 4.4) + 20
+        } else {
+          if (ball.y + ball.radius >= height) {
+            ball.velY *= -ball.bounce
+            ball.y = height - ball.radius
+            ball.velX *= friction
+          }
+          if (ball.y - ball.radius <= 0) {
+            ball.velY *= -ball.bounce
+            ball.y = ball.radius
+            ball.velX *= friction
+          }
+          if (ball.x - ball.radius <= 0) {
+            ball.velX *= -ball.bounce
+            ball.x = ball.radius
+          }
+          if (ball.x + ball.radius >= width) {
+            ball.velX *= -ball.bounce
+            ball.x = width - ball.radius
+          }
+  
+          if (ball.velX < 0.01 && ball.velX > -0.01) {
+            ball.velX = 0
+          }
+          if (ball.velY < 0.01 && ball.velY > -0.01) {
+            ball.velY = 0
+          }
+  
+          ball.x += ball.velX
+          ball.y += ball.velY
+        }
+        drawBall(ball)
+      })
+    }
+    requestRef.current = requestAnimationFrame(update)
+  }
+  
   React.useEffect(() => {
+    const ctx = canvasRef.current.getContext('2d')
+    
+    if (ctx) {
+      let balls = []
+      
+      for (let i = 0; i < filteredAddresses.length; i++) {
+        const g = gradient(),
+        radius = 30 * (1+3*(filteredAddresses.length-i)/filteredAddresses.length);
+        const ball = {
+          address: filteredAddresses[i],
+          bounce: 1,
+          radius: radius,
+          originalRadius: radius,
+          x: canvasRef.current.width / 2,
+          y: canvasRef.current.height / 2,
+          velX: getRandomVel(),
+          velY: getRandomVel(),
+          colorStart: g.start,
+          colorEnd: g.end
+        }
+        balls.push(ball);
+      }
 
-    // Specify how to clean up after this effect:
+      if (!!currentSelected) {
+        // Initialize any selected address
+        const ball = balls.find(ball => ball.address === currentSelected)
+        if (!!ball) {
+          ball.clicked = true
+        }
+      }
+
+      setContext(ctx)
+      ballsRef.current = balls
+      requestRef.current = requestAnimationFrame(update)
+
+    }
+
     return function cleanup() {
-      if (this.req) {
-        cancelAnimationFrame(this.req);
+      if (requestRef) {
+        cancelAnimationFrame(requestRef.current);
       }
     }
 
-  }, [filteredAddresses, currentSelected]);
+  }, [width, height, filteredAddresses, currentSelected]);
 
   return [];
 }
 
-export default function BoardAnimationCanvas({width, height}) {
-  const ref = React.useRef();
-  const [friction, setFriction] = React.useState(0.98);
-  const [balls, setBalls] = React.useState([]);
-  const [ctx, setCtx] = React.useState();
-
-  
+export default function BoardAnimationCanvas({width, height, topY, onBallClick}) {
+  const canvasRef = React.useRef();
+  const ballsRef = React.useRef();
   const selectedChain = useSelector(selectChain);
   const currentSelected = useSelector(selectAddress);
   const selectedPage = useSelector(selectPage);
-  const filteredAddresses = ['a', 'b', 'c'];
+  const filteredAddresses = ['a'];
 
-  useInitCanvas(ref, width, height);
-  useUpdateCanvas(ref, selectedChain, filteredAddresses, currentSelected);
+  useInitCanvas(canvasRef, ballsRef, width, height, topY, onBallClick);
+  useUpdateCanvas(canvasRef, ballsRef, width, height, filteredAddresses, currentSelected);
   
 
   const handleOnBallClick = (address) => {
@@ -217,7 +284,7 @@ export default function BoardAnimationCanvas({width, height}) {
       backgroundColor: "rgba(241, 241, 240, 0.95)",
       borderBottom: `solid 2px #FFF`,
       }}>
-        <canvas ref={ref}
+        <canvas ref={canvasRef}
           style={{
             padding: 0,
             margin: "0 auto",
--- src/components/nomi/DashboardPage.js
+++ src/components/nomi/DashboardPage.js
@@ -1,19 +1,37 @@
 import * as React from 'react';
-import { useSelector } from 'react-redux';
-import { useTheme } from '@mui/material/styles';
-import Grid from '@mui/material/Grid';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import Box from '@mui/material/Box';
 import BoardAnimationCanvas from './BoardAnimationCanvas';
-
+import { isValidAddress } from '../../util/crypto'
+import {
+  addressChanged,
+  selectChain,
+  selectAddress
+} from '../../features/chain/chainSlice';
 
 export default function DashboardPage() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleOnBallClick = (address) => {
+    console.log("TODO__handleOnBallClick ");
+    if (isValidAddress(address)) {
+      dispatch(addressChanged(address));
+      navigate({
+        search: `?a=${address}`,
+      })
+    }
+  }
+
   return (
-    <Box sx={{ mb: 10, display: 'flex', justifyContent: 'center'}}>
-        <Box sx={{ height: '95vh', position: 'relative'}}>
+    <Box sx={{ mb: 10, display: 'flex', justifyContent: 'center', backgroundColor: '#3D3D3D'}}>
+        <Box sx={{ height: '95vh', position: 'relative', backgroundColor: '#3D3D3D'}}>
           <BoardAnimationCanvas 
-            width={window.innerWidth} 
+            width={window.innerWidth - 56} 
             height={window.innerHeight * 0.95}
-            topY={496} // heroBox height = 496
+            topY={64}
+            onBallClick={handleOnBallClick}
           />
           {/* <AccountInfoTable onClose={this.handleOnAccountInfoClose} />
           <Leaderboard /> */}
